35195e8fb1021eaf1207d283f48d2b134843f4a2
eljam3239 <eljam3239@7KTM6Y1.Student.UGDSB.ED>
2017-02-28 13:34:09 -0500

    area of a rectangle

---
+++ src/areaofarectangle/AreaOfARectangle.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package areaofarectangle;
+
+/**
+ *
+ * @author eljam3239
+ */
+public class AreaOfARectangle {
+
+   
+    public static void main(String[] args) {
+        double len = 4.8;
+        double wid = 5.7;
+        System.out.println(wid*len);
+    }
+    
+}
